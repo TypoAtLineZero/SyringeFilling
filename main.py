6692aa145057c92483533ef7e49fe406ab3e9ebc
TypoAtLineZero <102697906+TypoAtLineZero@users.noreply.github.com>
2023-05-23 23:29:20 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ ledR = 26
 
 
 ## Stepper motor Definition
-step_sleep = 0.00075
+step_sleep = 0.001
 step_count = 4096
 direction = True
 step_sequence = [[1,0,0,1],
@@ -68,15 +68,69 @@ def cleanup():
     GPIO.output(ledR, GPIO.LOW)
 
 
+def turnMotor(direction, step_count, step_sleep):
+    
+    print("direction:", direction)
+    print("step_count:", step_count)
+    print("step_sleep:", step_sleep)
+    
+    motor_step_counter = 0
+    i = 0
+    
+    for i in range(step_count):
+        for pin in range(0, len(motor_pins)):
+            GPIO.output(motor_pins[pin], step_sequence[motor_step_counter][pin])
+        if direction == True:
+            motor_step_counter = (motor_step_counter - 1) % 8
+        elif direction == False:
+            motor_step_counter = (motor_step_counter + 1) % 8
+        else:
+            print("diretion not boolean")
+            cleanup()
+            exit(1)
+        time.sleep(step_sleep)
+
 
 def buttonGreenPressed(channel):
-    print("Green Pressed!")
+    ## State: Turn forward for longer period
+    switchLeds(ledG, ledR)
+    direction_green = True
+    step_count_green = step_count * 2
+    step_sleep_green = step_sleep * 0.75
+    
+    turnMotor(direction_green, step_count_green, step_sleep_green)
+    print("Finished my turn!")
+    switchLeds(ledR, ledG)
+
 
 def buttonRedPressed(channel):
-    print("Red Pressed!")
+    ## State: Turn forward for shorter period
+    switchLeds(ledG, ledR)
+    direction_red = True
+    step_count_red = step_count / 4
+    step_sleep_red = step_sleep * 2
+    
+    turnMotor(direction_red, int(step_count_red), step_sleep_red)
+    print("Finished my turn!")
+    switchLeds(ledR, ledG)
+    
     
 def buttonBluePressed(channel):
-    print("Blue Pressed!")
+    ## State: Turn backwards for shorter period
+    switchLeds(ledG, ledR)
+    direction_blue = False
+    step_count_blue = step_count / 4
+    step_sleep_blue = step_sleep * 2
+    
+    turnMotor(direction_blue, int(step_count_blue), step_sleep_blue)
+    print("Finished my turn!")
+    switchLeds(ledR, ledG)
+    
+    
+def switchLeds(LedOff, LedOn):
+    GPIO.output(LedOff, GPIO.LOW)
+    GPIO.output(LedOn, GPIO.HIGH)
+
 
 ## main
 if __name__ == '__main__':
@@ -85,31 +139,18 @@ if __name__ == '__main__':
     GPIO.add_event_detect(buttonGreenFwLong, GPIO.RISING, callback=buttonGreenPressed, bouncetime=500)
     GPIO.add_event_detect(buttonRedFwShort, GPIO.RISING, callback=buttonRedPressed, bouncetime=500)
     GPIO.add_event_detect(buttonBlueBwShort, GPIO.RISING, callback=buttonBluePressed, bouncetime=500)
-
-    try:
-        i = 0
-        for i in range(step_count):
-            for pin in range(0, len(motor_pins)):
-                GPIO.output(motor_pins[pin], step_sequence[motor_step_counter][pin])
-            if direction == True:
-                motor_step_counter = (motor_step_counter - 1) % 8
-                GPIO.output(ledG, GPIO.HIGH)
-                GPIO.output(ledY, GPIO.HIGH)
-                GPIO.output(ledR, GPIO.HIGH)
-            elif direction == False:
-                motor_step_counter = (motor_step_counter + 1) % 8
-            else:
-                print("diretion not boolean")
-                cleanup()
-                exit(1)
-            time.sleep(step_sleep)
-            
-    except KeyboardInterrupt:
-        cleanup()
-        exit(1)
-        
-    cleanup()
-    exit(0)
+    GPIO.output(ledY, GPIO.HIGH)
+    time.sleep(1)
+    switchLeds(ledY, ledG)
+    ## State: Ready for buttonXPressed
+    
+    while True:
+        try:
+            print("Waiting for input..\n")
+            time.sleep(5)
+        except KeyboardInterrupt:
+            cleanup()
+            exit(1)
 
 
 
